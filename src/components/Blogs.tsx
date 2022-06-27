03ba63c46947d5f43e78d07ac8c8344a2b2f791b
Ropepe <pericavrhovac@gmail.com>
2022-06-27 15:40:21 +0200

    Drugi commit

--- src/components/Blogs.tsx
+++ src/components/Blogs.tsx
@@ -3,7 +3,7 @@ import React from "react";
 interface IProps {}
 
 const Blogs: React.FC<IProps> = () => {
-  return <div>ROPEPE</div>;
+  return <div>Dio za blogove</div>;
 };
 
 export default Blogs;
