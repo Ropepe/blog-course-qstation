b2e165f33edda90532e303e9162dadeafc33e9ec
Ropepe <pericavrhovac@gmail.com>
2022-06-28 13:56:54 +0200

    tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Blogs from "./components/Blogs";
-
+import "./index.css";
 interface IProps {}
 
 const App: React.FC<IProps> = () => {
   return (
     <div>
-      <h1>Blog</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <Blogs />
     </div>
   );
--- src/components/Blogs.tsx
+++ src/components/Blogs.tsx
@@ -3,7 +3,12 @@ import React from "react";
 interface IProps {}
 
 const Blogs: React.FC<IProps> = () => {
-  return <div>Dio za blogove</div>;
+  return (
+    <div className="bg-teal-500">
+      <button className="bg-teal-500">Dio za blogove</button>
+      <h1 className="text-sm text-gray-500">Hello world!</h1>
+    </div>
+  );
 };
 
 export default Blogs;
